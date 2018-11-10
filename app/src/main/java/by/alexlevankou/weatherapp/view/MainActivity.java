ad416131967e9c34e1efdfdb6ddbec870e8e624c
Alex Levankou <alexlevankou@gmail.com>
2018-11-10 14:53:05 +0300

    Add images.

--- app/src/main/java/by/alexlevankou/weatherapp/view/MainActivity.java
+++ app/src/main/java/by/alexlevankou/weatherapp/view/MainActivity.java
@@ -13,9 +13,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-
 
 import by.alexlevankou.weatherapp.R;
 import by.alexlevankou.weatherapp.model.WeatherData;
@@ -24,12 +22,10 @@ import by.alexlevankou.weatherapp.viewmodel.WeatherViewModel;
 
 public class MainActivity extends AppCompatActivity implements LocationListener{
 
-    private LocationManager locationManager = null;
+    private LocationManager mLocationManager = null;
     private Location mLocation;
     private WeatherViewModel mViewModel;
     //private GPSTracker gpsTrackerService;
-
-    boolean isLocationLoaded = false;
 
     static final int REQUEST_CODE_PERMISSION_ACCESS_LOCATION = 0;
 
@@ -45,7 +41,6 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_CODE_PERMISSION_ACCESS_LOCATION);
             return;
         }
-
         requestLocation();
     }
 
@@ -56,20 +51,8 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
             case REQUEST_CODE_PERMISSION_ACCESS_LOCATION:
                 if (grantResults.length >= 2 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
                     requestLocation();
-//                    mViewModel.getWeatherByCoordinates(mLocation.getLatitude(), mLocation.getLongitude()).observe(this, new Observer<WeatherData>() {
-//                        @Override
-//                        public void onChanged(@Nullable WeatherData weatherData) {
-//                            int z = 9;
-//                        }
-//                    });
                 } else {
-                    // need to call data by city name
-                    mViewModel.getWeatherByCity("Minsk").observe(this, new Observer<WeatherData>() {
-                        @Override
-                        public void onChanged(@Nullable WeatherData weatherData) {
-                            int z = 9;
-                        }
-                    });
+                    // display no data
                 }
                 break;
             default:
@@ -84,27 +67,14 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
     @SuppressLint("MissingPermission")
     private void requestLocation()
     {
-        if(locationManager == null)
+        if(mLocationManager == null)
         {
-            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         }
-        if(locationManager != null)
+        if(mLocationManager != null)
         {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    private void getLocation()
-    {
-        if(locationManager == null)
-        {
-            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        }
-        if(locationManager != null)
-        {
-            mLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
         }
     }
 
@@ -115,21 +85,16 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
     }
 
     public void onLocationChanged(Location location) {
-        if(!isLocationLoaded)
-        {
-            mLocation = location;
-            mViewModel.getWeatherByCoordinates(mLocation.getLatitude(), mLocation.getLongitude()).observe(this, new Observer<WeatherData>() {
-                @Override
-                public void onChanged(@Nullable WeatherData weatherData) {
-                    int z = 9;
-                }
-            });
-            isLocationLoaded = true;
-        }
+        mLocation = location;
+        mViewModel.getWeatherByCoordinates(mLocation.getLatitude(), mLocation.getLongitude()).observe(this, new Observer<WeatherData>() {
+            @Override
+            public void onChanged(@Nullable WeatherData weatherData) {
+                int z = 9;
+            }
+        });
     }
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
-
     }
 }
